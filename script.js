8e184d8bb3c424e531a34d68723d5b4d1d2030c2
ClaytonxParrish <105694697+ClaytonxParrish@users.noreply.github.com>
2023-06-06 19:34:12 -0700

    new features for adding and subtracting

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const plusBtn = document.getElementById('plus')
 const minusBtn = document.getElementById('minus')
 const plusFiveBtn = document.getElementById('plus-five')
 const minusFiveBtn = document.getElementById('minus-five')
-
+const incrementOdd = document.getElementById('increment-odd')
+const incrementAsync = document.getElementById('increment-async')
+const incrementCustonBtn = document.getElementById('increment-custom')
+const customValue = document.getElementById('custom-value')
 // initial state value
 const initialState = {
     value: 0
@@ -12,18 +15,20 @@ const initialState = {
 
 // reducer
 const counterReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'counter/incremented':
-            return { value: state.value + 1 }
-        case 'counter/decremented':
-            return { value: state.value - 1 }
-        case 'counter/incrementedFive':
-            return { value: state.value + 5 }
-        case 'counter/decrementedFive':
-            return { value: state.value - 5 }
-        default:
-        return state
-    }
+  switch (action.type) {
+      case 'counter/incremented':
+          return { value: state.value + 1 }
+      case 'counter/decremented':
+          return { value: state.value - 1 }
+      case 'counter/incrementedFive':
+          return { value: state.value + 5 }
+      case 'counter/decrementedFive':
+          return { value: state.value - 5 }
+      case 'counter/custom':
+          return { value: state.value + action.payload }
+      default:
+      return state
+  }
 }
 
 // action object definitions
@@ -69,11 +74,33 @@ const subFive = () => {
   store.dispatch(subFiveAction)
 }
 
+const incrementIfOdd = () => {
+  console.log(store.getState())
+  if(store.getState().value % 2 !== 0) {
+    store.dispatch(addAction)
+  }
+}
+
+const incrementOneSecond = () => {
+  setTimeout(() => {
+    store.dispatch(addAction)
+  }, 1000)
+}
+
+const changeCustom = () => {
+store.dispatch({
+  type: 'counter/custom',payload: (+customValue.value)
+})
+}
+
 // event listeners
 plusBtn.addEventListener('click', addOne)
 minusBtn.addEventListener('click', subOne)
 plusFiveBtn.addEventListener('click', addFive)
 minusFiveBtn.addEventListener('click', subFive)
+incrementOdd.addEventListener('click', incrementIfOdd)
+incrementAsync.addEventListener('click', incrementOneSecond)
+incrementCustonBtn.addEventListener('click', changeCustom)
 
 // initial render
 render()
